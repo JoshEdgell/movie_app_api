a4b9f04ce31a40480c4d155093da05b63b0be6f2
Richard Neely <richardneely@Richards-Mac-Pro.local>
2017-09-09 10:45:44 -0400

    added imdb_id ot movies schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908002204) do
+ActiveRecord::Schema.define(version: 20170909143746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170908002204) do
     t.string "rotten_tomatoes_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "imdb_id"
   end
 
   create_table "reviews", force: :cascade do |t|
