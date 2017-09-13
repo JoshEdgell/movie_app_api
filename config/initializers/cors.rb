ee6b2c8ea48798272e5db467b2df9f46a7248104
Josh Edgell <josh.0.edgell@gmail.com>
2017-09-13 13:01:00 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -18,6 +18,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :options, :head]
   end
 end
