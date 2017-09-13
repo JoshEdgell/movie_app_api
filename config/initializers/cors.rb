2091e98e1e8156ed9acd069476fb9a2d3d4cddfe
Josh Edgell <josh.0.edgell@gmail.com>
2017-09-13 12:37:24 -0400

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'moviereviewr.heroku.com'
+    origins ['moviereviewr.herokuapp.com', 'localhost:3001']
 
     resource '*',
       headers: :any,
